cb40316f4b91e64cec493442c10ed77433e66207
Kouame N'Dri <kouamendri@outlook.com>
2019-04-14 17:09:43 -0400

    completed home screen

--- screens/Home/index.js
+++ screens/Home/index.js
@@ -4,31 +4,80 @@ import styles from './styles'
 import { Header } from '../../components'
 
 export default class Home extends Component {
+  state = {
+    isSoundOn: true
+  };
 
   onPlayPress = () => {
     console.log("onPlayPress event handler");
   };
 
-  render () {
+  onLeaderboardPress = () => {
+    console.log("onLeaderboardPress event handler");
+  };
+
+  onToggleSound = () => {
+    this.setState({ isSoundOn: !this.state.isSoundOn });
+  };
+
+  render() {
+    const imageSource = this.state.isSoundOn
+    ? require("../../assets/icons/speaker-on.png")
+    : require("../../assets/icons/speaker-off.png");
+    
     return (
       <View style={styles.container}>
         <Header />
+
         {/* Play Button */}
-        <TouchableOpacity onPress={this.onPlayPress} style={{ flexDirection: 'row', alignItems: 'center' }}>
+        <TouchableOpacity onPress={this.onPlayPress} style={{ flexDirection: 'row', alignItems: 'center', marginTop: 80 }}>
           <Image
             source={require("../../assets/icons/play_arrow.png")}
             style={styles.playIcon}
           />
           <Text style={styles.play}>PLAY!</Text>
         </TouchableOpacity>
+        {/* End Play Button */}
+
         {/* Hi-Score */}
-        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+        <View style={{ flexDirection: 'row', alignItems: 'center', marginTop: 80 }}>
           <Image
             source={require("../../assets/icons/trophy.png")}
             style={styles.trophyIcon}
           />
           <Text style={styles.hiscore}>Hi-score: 0</Text>
         </View>
+        {/* End Hi-Score */}
+
+        {/* Leaderboard */}
+        <TouchableOpacity onPress={this.onLeaderboardPress} style={{ flexDirection: 'row', alignItems: 'center', marginTop: 80}}>
+          <Image
+            source={require("../../assets/icons/leaderboard.png")}
+            style={styles.leaderboardIcon}
+          />
+          <Text style={styles.leaderboard}>Leaderboard</Text>
+        </TouchableOpacity>
+        {/* End Leaderboard */}
+        
+        {/* Copyright Banner */}
+        <View style={styles.bottomContainer}>
+          <View>  
+            <Text style={[styles.copyrightText, { color: "#E64C3C" }]}>
+              Music: Komiku
+            </Text>
+            <Text style={[styles.copyrightText, { color: "#F1C431" }]}>
+              SFX: SubspaceAudio
+            </Text>
+            <Text style={[styles.copyrightText, { color: "#3998DB" }]}>
+              Development: RisingStack
+            </Text>
+            </View>
+            <View style={{ flex: 1 }} />
+            <TouchableOpacity onPress={this.onToggleSound}>
+              <Image source={imageSource} style={styles.soundIcon} />
+            </TouchableOpacity>
+        </View>
+        {/* End Copyright Banner */}
       </View>
     )
   }
--- screens/Home/styles.js
+++ screens/Home/styles.js
@@ -18,6 +18,17 @@ export default StyleSheet.create({
     width: 60,
     marginRight: 15
   },
+  leaderboard: {
+    fontSize: 40,
+    fontFamily: 'dogbyte',
+    color: '#ecf0f1',
+    marginTop: 5
+  },
+  leaderboardIcon: {
+    height: 50,
+    width: 50,
+    marginRight: 15
+  },
   hiscore: {
     fontSize: 28.5,
     fontFamily: 'dogbyte',
@@ -28,5 +39,21 @@ export default StyleSheet.create({
     height: 45,
     width: 45,
     marginRight: 12.5
+  },
+  bottomContainer: {
+    position: 'absolute',
+    left: 15,
+    right: 15,
+    bottom: 12.5,
+    flexDirection: 'row'
+  },
+  copyrightText: {
+    fontSize: 16,
+    fontFamily: 'dogbyte',
+    marginBottom: 2.5
+  },
+  soundIcon: {
+    height: 35,
+    width: 35
   }
 })
